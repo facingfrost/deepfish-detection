53b00736d636dbc738af2a5baaa54cc067b6f25d
GianCarloTG <giantejgar@gmail.com>
2024-10-02 03:25:04 +0200

    contrast and histogram added

--- preprocessing_img.py
+++ preprocessing_img.py
@@ -1,28 +1,54 @@
 from roboflow import Roboflow
-from PIL import ImageEnhance, Image
+from PIL import ImageEnhance, Image, ImageOps 
 import os
+import re
+import numpy as np
 
+def img_contrast(image):
+    enhancer = ImageEnhance.Contrast(image)
+    img_with_contrast = enhancer.enhance(1.5)
+    return img_with_contrast
 
-def enhance_img(api_key, workspace, input_path, output_path):
-    # enhances the img from the input folder
-    # and saves it in an output folder
-    rf = Roboflow(api_key=api_key)
-    project = rf.workspace(workspace).project(workspace)
-    model = project.version(1).model
+def img_histogram(image):
+    return ImageOps.equalize(image)
+
+def img_adapt(input_path):
+    '''missing Adapative Equalization'''
+    pass
+
+def enhance_img(input_path, output_path, method = 'all'):
+    ''' enhances the img from the input folder
+        and saves it in an output folder  
+        check = all(value == 'independent' for value in dict_facilities.values())
+        method: contrast, histogram, adaptative, all (3 combined)'''
     image = Image.open(input_path)
-    enhancer = ImageEnhance.Contrast(image) #method to use
-    image_with_contrast = enhancer.enhance(1.5) #hyperparameter
-    image_with_contrast.save(output_path)
+    regex = re.search(r'(?<!\/)\/(?!.*\/)', input_path)
+    file_name = regex.start()
+    output = output_path + input_path[file_name:][:-4]
+    
+    if method == 'contrast':
+        img_with_contrast = img_contrast(image)
+        img_with_contrast.save(output+ '_contrast.jpg')
+
+    elif method == 'histogram':
+        img_equalized = img_histogram(image)
+        img_equalized.save(output+ '_histogram.jpg')
+
+    elif method == 'adaptative':
+        pass
+
+    elif method == 'all':
+        img_with_contrast = img_contrast(image)
+        img_equalized= img_histogram(img_with_contrast)
+        img_adaptative = img_adapt(img_equalized)
+        img_adaptative.save(output+ '_all.jpg')
 
 
 def main():
-    api_key =  ''
     input_path = './datasets/images/train/'
     output_path = './datasets/images_preprocessed/'
-    workspace = 'dc3'
     for image in os.listdir(input_path):
-        output = output_path + image[:-4] + '_contrast.jpg'
-        enhance_img(api_key, workspace, input_path+image, output)
+        enhance_img(input_path+image, output_path, 'all')
 
 if __name__ == "__main__":
     main()
